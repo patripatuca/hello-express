68ecafbe57d2d832808487e50243e5ed773c668b
afd <afd@alup5.academiapostal.es.local>
2020-01-29 19:35:22 +0100

    refactorizacion . productos en modulo aparte

---
+++ models/products.js
@@ -0,0 +1,6 @@
+module.exports=[
+    {nombre:"Microondas",precio:45,existencias:6,imagen:"microondas.jpg"},
+    {nombre:"frigorifico",precio:200,existencias:4,imagen:"frigorifico.jpg"},
+    {nombre:"Lámpara",precio:20,existencias:14,imagen:"Lámpara.jpg"},
+    {nombre:"Tv",precio:90,existencias:0,imagen:"TV.jpg"}
+  ];
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var products=require("../models/products.js")
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const products=[
-    {nombre:"Microondas",precio:45,existencias:6},
-    {nombre:"frigorifico",precio:200,existencias:4},
-    {nombre:"Lámpara",precio:20,existencias:14},
-    {nombre:"Tv",precio:90,existencias:0}
-  ];
+  
+    
   res.render('index', { title: 'to home patri' ,products});
 });
 
