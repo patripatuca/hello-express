f56b9c779f7ac7d13a55f049b8c6c714f73f326b
afd <afd@alup5.academiapostal.es.local>
2020-02-28 19:43:07 +0100

    modificaciones en users.js

--- models/users.js
+++ models/users.js
@@ -2,10 +2,9 @@ const Sequelize=require('sequelize');
 const sequelize=require('./db');
 //definimos el usuario 
 const Usuario = sequelize.define('usuario', {
-  nombre:Sequelize.STRING(50),
-  apellidos:Sequelize.STRING(80),
+  nombre:{type:Sequelize.STRING(50),allowNull:false},
+  apellidos:{type:Sequelize.STRING(80),allowNull:false},
   email:{type:Sequelize.STRING(100),allowNull:false,unique:true},
-  password:Sequelize.STRING(50)
-  
+  password:{type:Sequelize.STRING(50),allowNull:false} 
 });
 module.exports=Usuario;
